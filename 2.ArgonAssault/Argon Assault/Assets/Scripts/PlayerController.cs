9d8b9b3ce81ac4eec295886d10002922ab75cde8
Bob Flynn <BFlynn@uwcu.org>
2018-11-05 16:35:09 -0600

    add slackbar, some polish, and timelines

--- 2.ArgonAssault/Argon Assault/Assets/Scripts/PlayerController.cs
+++ 2.ArgonAssault/Argon Assault/Assets/Scripts/PlayerController.cs
@@ -48,27 +48,20 @@ public class PlayerController : MonoBehaviour {
         if (CrossPlatformInputManager.GetButton("Fire"))
         {
             //print("Firing");
-            ActivateGuns();
+            ActivateGuns(true);
         }
         else
         {
-            DeactivateGuns();
+            ActivateGuns(false);
         }
     }
 
-    private void ActivateGuns()
+    private void ActivateGuns(bool isActive)
     {
         foreach (var gun in guns)
         {
-            gun.SetActive(true);
-        }
-    }
-
-    private void DeactivateGuns()
-    {
-        foreach (var gun in guns)
-        {
-            gun.SetActive(false);
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
         }
     }
 
