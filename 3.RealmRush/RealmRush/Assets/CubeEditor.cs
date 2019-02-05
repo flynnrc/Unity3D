0a8f9a134d14a2f61414f2dac5f499505f284b18
Robert Flynn <flynnrc@uwec.edu>
2019-02-05 00:24:53 -0600

    s135 initial pathfinding

--- 3.RealmRush/RealmRush/Assets/CubeEditor.cs
+++ 3.RealmRush/RealmRush/Assets/CubeEditor.cs
@@ -30,14 +30,17 @@ public class CubeEditor : MonoBehaviour {
     private void UpdateLabel()
     {
         int gridSize = waypoint.GetGridSize();
-        string labelText = waypoint.GetGridPos().x/ gridSize + "," + waypoint.GetGridPos().y/ gridSize;
+        string labelText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
         textMesh.text = labelText;
         gameObject.name = "Cube " + labelText;
     }
 
     private void SnapToGrid()
     {
-        //int gridSize = waypoint.GetGridSize();
-        transform.position = new Vector3(waypoint.GetGridPos().x, 0f, waypoint.GetGridPos().y);
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x * gridSize, 
+            0f, 
+            waypoint.GetGridPos().y * gridSize);
     }
 }
--- 3.RealmRush/RealmRush/Assets/Waypoint.cs
+++ 3.RealmRush/RealmRush/Assets/Waypoint.cs
@@ -6,17 +6,24 @@ public class Waypoint : MonoBehaviour {
 
     const int gridSize = 10;
     Vector2Int gridPos;
+    public bool isExplored = false;
 
     public int GetGridSize()
     {
         return gridSize;
     }
 
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {
         return new Vector2Int(
-            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
-            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            Mathf.RoundToInt(transform.position.x / gridSize),
+            Mathf.RoundToInt(transform.position.z / gridSize)
         );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRend = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRend.material.color = color;
     }
 }
