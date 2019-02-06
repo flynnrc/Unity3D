55e35144f4a8a2c13b2737d533d16cc5895996dd
Robert Flynn <flynnrc@uwec.edu>
2019-02-06 00:39:40 -0600

    s142 pathfinding and towers

---
+++ 3.RealmRush/RealmRush/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]//basically sets it up so the script will run in edit mode as it would in play mode
+[SelectionBase]//sets this object as the base object for clicking, handy to avoid clicking children when all you want is the parent
+[RequireComponent(typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour {
+
+    Waypoint waypoint;
+    TextMesh textMesh;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = "xx,xx";
+    }
+
+    void Update ()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void UpdateLabel()
+    {
+        //int gridSize = waypoint.GetGridSize();
+        string labelText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
+        textMesh.text = labelText;
+        gameObject.name = "Cube " + labelText;
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x * gridSize, 
+            0f, 
+            waypoint.GetGridPos().y * gridSize);
+    }
+}
---
+++ 3.RealmRush/RealmRush/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+    const int gridSize = 10;
+    Vector2Int gridPos;
+    [SerializeField] Color exploredColor;
+    [SerializeField] Color unexploredColor;
+
+    public bool isExplored = false;
+    public Waypoint previousWaypoint;
+
+    private void Update()
+    {
+        if (isExplored)
+        {
+            SetTopColor(exploredColor);
+        }
+    }
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize),
+            Mathf.RoundToInt(transform.position.z / gridSize)
+        );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRend = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRend.material.color = color;
+    }
+}
