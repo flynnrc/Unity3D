0aa2eb8ecbb3eeb837401d7ca5b7db0c5bc2b438
Robert Flynn <flynnrc@uwec.edu>
2019-02-26 00:28:51 -0600

    more test cameras and playgrounds

---
+++ 4.RPG/Dragon RPG/Assets/CameraFollowV2.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Pairs up with camera collision
+/// </summary>
+public class CameraFollowV2 : MonoBehaviour
+{
+
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject CameraFollowObj;
+    Vector3 FollowPOS;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public float mouseX;
+    public float mouseY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            // We setup the rotation of the sticks here
+            mouseX = Input.GetAxis("Mouse X");
+            mouseY = Input.GetAxis("Mouse Y");
+
+            rotY += mouseX * inputSensitivity * Time.deltaTime;
+            rotX += mouseY * inputSensitivity * Time.deltaTime;
+
+            rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+            Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+            transform.rotation = localRotation;
+        }
+
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = CameraFollowObj.transform;
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ 4.RPG/Dragon RPG/Assets/CameraFollow_Test.cs
@@ -0,0 +1,101 @@
+﻿//using System.Collections;
+
+//using System.Collections.Generic;
+
+//using UnityEngine;
+
+
+
+//public class CameraFollow_Test : MonoBehaviour
+
+//{
+
+//    public GameObject Player;
+
+//    public float JoyAngX, JoyAngY;
+
+//    public float InputSensit = 155.0f;
+
+//    public float RotY = 0.0f, RotX = 0.0f;
+
+//    public float LimiteAng = 65.0f;
+
+//    private Quaternion LocalRot;
+
+//    // Start is called before the first frame update
+
+//    void Start()
+
+//    {
+
+//        Player = GameObject.FindGameObjectWithTag("Player");
+
+//    }
+
+
+
+//    // Update is called once per frame
+
+//    void LateUpdate()
+
+//    {
+
+//        transform.position = Player.transform.position;
+
+//    }
+
+
+
+//    void Update()
+
+//    {
+
+//        transform.position = Player.transform.position;
+
+//        Atualizacao();
+
+//    }
+
+
+
+//    void Atualizacao()
+
+//    {
+
+//        if (PlayerMovement.isInDirectMode)
+
+//        {
+
+//            JoyAngX = Input.GetAxis("CameraRot");
+
+//            JoyAngY = Input.GetAxis("CameraRot2");
+
+//        }
+
+//        else
+
+//        {
+
+//            JoyAngX = Input.GetAxis("Mouse X");
+
+//            JoyAngY = Input.GetAxis("Mouse Y");
+
+//        }
+
+
+
+//        RotY += JoyAngX * InputSensit * Time.deltaTime;
+
+//        RotX += (-JoyAngY) * InputSensit * Time.deltaTime;
+
+
+
+//        RotX = Mathf.Clamp(RotX, -LimiteAng, LimiteAng);
+
+//        LocalRot = Quaternion.Euler(RotX, RotY, 0);
+
+//        transform.rotation = LocalRot;
+
+//    }
+
+//}
