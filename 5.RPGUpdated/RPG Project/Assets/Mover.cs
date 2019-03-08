d84c54f967a824f66912f6dc6330b97ae236f43f
Robert Flynn <flynnrc@uwec.edu>
2019-03-07 23:42:37 -0600

    2018.3.7f1 sandbox RPG Proj, begin nav mesh

---
+++ 5.RPGUpdated/RPG Project/Assets/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = target.transform.position;
+    }
+}
