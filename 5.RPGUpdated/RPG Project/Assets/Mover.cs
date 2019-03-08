9a123cb250a1624be48d36929b7d96cb115286c0
Robert Flynn <flynnrc@uwec.edu>
2019-03-07 23:55:40 -0600

    init raycast debug

--- 5.RPGUpdated/RPG Project/Assets/Mover.cs
+++ 5.RPGUpdated/RPG Project/Assets/Mover.cs
@@ -8,6 +8,8 @@ public class Mover : MonoBehaviour
     [SerializeField] GameObject target;
     private NavMeshAgent navMeshAgent;
 
+    Ray lastRay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,11 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);//100 extends ray
         navMeshAgent.destination = target.transform.position;
     }
 }
