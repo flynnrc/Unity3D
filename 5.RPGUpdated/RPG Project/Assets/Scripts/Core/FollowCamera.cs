e6a2064f3a0fedcaec0211af21b14717c8382821
Robert Flynn <flynnrc@uwec.edu>
2019-03-10 19:44:54 -0500

    basic movement s17

---
+++ 5.RPGUpdated/RPG Project/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform followTarget = null;
+
+    // Update is called once per frame
+    void LateUpdate()//camera should move after animations (https://docs.unity3d.com/Manual/ExecutionOrder.html)
+    {
+        transform.position = followTarget.position;
+    }
+}
