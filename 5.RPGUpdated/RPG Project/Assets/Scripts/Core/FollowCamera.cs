e4fe99f72d958a98a43fa12e9bfb68cf63683fdd
Robert Flynn <flynnrc@uwec.edu>
2019-03-12 23:35:22 -0500

    interfaces

--- 5.RPGUpdated/RPG Project/Assets/Scripts/Core/FollowCamera.cs
+++ 5.RPGUpdated/RPG Project/Assets/Scripts/Core/FollowCamera.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+namespace RPG.Core
 {
-    [SerializeField] Transform followTarget = null;
-
-    // Update is called once per frame
-    void LateUpdate()//camera should move after animations (https://docs.unity3d.com/Manual/ExecutionOrder.html)
+    public class FollowCamera : MonoBehaviour
     {
-        transform.position = followTarget.position;
+        [SerializeField] Transform followTarget = null;
+
+        // Update is called once per frame
+        void LateUpdate()//camera should move after animations (https://docs.unity3d.com/Manual/ExecutionOrder.html)
+        {
+            transform.position = followTarget.position;
+        }
     }
 }
--- 5.RPGUpdated/RPG Project/Assets/Scripts/Movement/Mover.cs
+++ 5.RPGUpdated/RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -1,40 +1,48 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using RPG.Combat;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    [SerializeField] GameObject target;
-
-    // Update is called once per frame
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        if (Input.GetMouseButton(0))
+        NavMeshAgent navMeshAgent;
+
+        private void Start()
         {
-            MoveToCursor();
+            navMeshAgent = GetComponent<NavMeshAgent>();
         }
 
-        UpdateAnimator();
-    }
-
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+        void Update()
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            UpdateAnimator();
         }
-    }
 
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("ForwardSpeed", Mathf.Abs(speed));
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
     }
 }
