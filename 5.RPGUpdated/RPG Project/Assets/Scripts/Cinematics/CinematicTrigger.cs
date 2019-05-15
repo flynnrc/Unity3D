afb78cd592e3985952051034a9e72f382f59a6aa
Robert Flynn <flynnrc@uwec.edu>
2019-05-14 22:35:04 -0500

    s4 s.63 trigger cutscene

---
+++ 5.RPGUpdated/RPG Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+
+        bool alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (!alreadyTriggered && collision.gameObject.tag == "Player")
+            {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
